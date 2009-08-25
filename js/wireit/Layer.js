bc256f00e960d3d4a1572ce9cb689089d0cfbe25
Noah Paessel <knowuh@gmail.com>
2009-08-25 16:04:23 -0400

    Should look at debug method, seems to be buggy! :)

--- js/wireit/Layer.js
+++ js/wireit/Layer.js
@@ -298,18 +298,18 @@ WireIt.Layer.prototype = {
     * @param {Object} wiring layer configuration
     */
    setWiring: function(wiring) {
-	  // console.log("removing all containers");
+	  // debug("removing all containers");
       this.removeAllContainers();
       
       if(YAHOO.lang.isArray(wiring.containers)) {
-    	  // console.log("got containers array");
+    	  // debug("got containers array");
          for(var i = 0 ; i < wiring.containers.length ; i++) {
             this.addContainer(wiring.containers[i]);
          }
       }
       if(YAHOO.lang.isArray(wiring.wires)) {
          for(var i = 0 ; i < wiring.wires.length ; i++) {
-        	// console.log("got wires array");
+        	// debug("got wires array");
             this.addWire(wiring.wires[i]);
          }
        }
