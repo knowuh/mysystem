1b6b8e4bdbf40242bf6b1a513e4f19027cad05d5
Noah Paessel <knowuh@gmail.com>
2009-08-25 18:17:11 -0400

    Better debug method.

--- js/MySystemUtil.js
+++ js/MySystemUtil.js
@@ -1,21 +1,22 @@
-function debug(message) {
-  // if (console && console.log) {
-  //   //console.log(message);
-  // } 
-}
-
-// let the monkey patching begin!
-WireIt.Layer.prototype.setOptions = function(options) {
-  var defaults = {
-    className: 'WireIt-Layer',
-    parentEl: document.body,
-    containers: [],
-    wires: [],
-    layerMap: false,
-    enableMouseEvents: true
+(function() {
+  debug = function(message) {
+    window.console && console.log && console.log(message);
   }
-  defaults = $H(defaults);
-  this.options = defaults.merge($H(options)).toObject();
-  debug("loaded defaults for wire-it layer");
-  debug(this.options.parentEl);
-};
+
+  if (window.WireIt && window.WireIt.Layer) {
+    WireIt.Layer.prototype.setOptions = function(options) {
+      var defaults = {
+        className: 'WireIt-Layer',
+        parentEl: document.body,
+        containers: [],
+        wires: [],
+        layerMap: false,
+        enableMouseEvents: true
+      }
+      defaults = $H(defaults);
+      this.options = defaults.merge($H(options)).toObject();
+      debug("loaded defaults for wire-it layer");
+      debug(this.options.parentEl);
+    };
+  }
+})();
