e9fee9f987fe35075784abf1e4537665183dc00d
Noah Paessel <knowuh@gmail.com>
2009-09-16 12:35:48 -0400

    Fixed / moved MySystemPrint to all Raphael implementation. * Arrows at the end of lines * support for scaling

--- js/MySystemPrint.js
+++ js/MySystemPrint.js
@@ -1,51 +1,153 @@
-(function() {
-
-
+(function () {
+  
+  // A bit hackish: If we don't want to load the rest of MySystem,
+  // we can create the namespace seperately:
+  MySystem = typeof(MySystem) != 'undefined' ? MySystem : function() {};
+  
+  
   /**
-  *
+  * Arrow 'plugin' for Raphael
   **/
-  Node = function() {
-    this.terminals = [];
-    this.icon = [];
-    this.x = 0;
-    this.y = 0;
+  Raphael.fn.arrow = function(startx,starty,endx,endy,len,angle,color) {
+    color = typeof(color) != 'undefined' ? color : "#888";
     
+    var theta = Math.atan2((endy-starty),(endx-startx));
+    var baseAngleA = theta + angle * Math.PI/180;
+    var baseAngleB = theta - angle * Math.PI/180;
+    var tipX = endx + len * Math.cos(theta);
+    var tipY = endy + len * Math.sin(theta);
+    var baseAX = endx - len * Math.cos(baseAngleA);
+    var baseAY = endy - len * Math.sin(baseAngleA);
+    var baseBX = endx - len * Math.cos(baseAngleB);
+    var baseBY = endy - len * Math.sin(baseAngleB);
+    var pathData = " M " + tipX      + " " + tipY +
+                   " L " + baseAX  + " " + baseAY +
+                   " L " + baseBX  + " " + baseBY +
+                   " Z ";
+    return {
+      path: this.path({fill: color, stroke: "none"},pathData),
+      type: 'arrow'
+    };
+  };
+  
+  /**
+  * Connection, taken from graffle.js:
+  * http://raphaeljs.com/graffle.js  author not cited, part of 
+  * rapheal project?  http://raphaeljs.com
+  **/
+  Raphael.fn.connection = function (obj1, obj2, line, bg) {
+      if (obj1.line && obj1.from && obj1.to) {
+          line = obj1;
+          obj1 = line.from;
+          obj2 = line.to;
+      }
+      var bb1 = obj1.getBBox();
+      var bb2 = obj2.getBBox();
+      var p = [{x: bb1.x + bb1.width / 2, y: bb1.y - 1},
+          {x: bb1.x + bb1.width / 2, y: bb1.y + bb1.height + 1},
+          {x: bb1.x - 1, y: bb1.y + bb1.height / 2},
+          {x: bb1.x + bb1.width + 1, y: bb1.y + bb1.height / 2},
+          {x: bb2.x + bb2.width / 2, y: bb2.y - 1},
+          {x: bb2.x + bb2.width / 2, y: bb2.y + bb2.height + 1},
+          {x: bb2.x - 1, y: bb2.y + bb2.height / 2},
+          {x: bb2.x + bb2.width + 1, y: bb2.y + bb2.height / 2}];
+      var d = {}, dis = [];
+      
+      for (var i = 0; i < 4; i++) {
+          var dx, dy;
+          for (var j = 4; j < 8; j++) {
+                  dx = Math.abs(p[i].x - p[j].x);
+                  dy = Math.abs(p[i].y - p[j].y);
+              if ((i == j - 4) || (((i != 3 && j != 6) || p[i].x < p[j].x) && ((i != 2 && j != 7) || p[i].x > p[j].x) && ((i != 0 && j != 5) || p[i].y > p[j].y) && ((i != 1 && j != 4) || p[i].y < p[j].y))) {
+                  dis.push(dx + dy);
+                  d[dis[dis.length - 1]] = [i, j];  
+              }
+          }
+      }
+      var res;
+      if (dis.length == 0) {
+          res = [0, 4];
+      } else {
+          res = d[Math.min.apply(Math, dis)];
+      }
+      var x1 = p[res[0]].x,
+          y1 = p[res[0]].y,
+          x4 = p[res[1]].x,
+          y4 = p[res[1]].y,
+          dx = Math.max(Math.abs(x1 - x4) / 2, 10),
+          dy = Math.max(Math.abs(y1 - y4) / 2, 10),
+          x2 = [x1, x1, x1 - dx, x1 + dx][res[0]].toFixed(3),
+          y2 = [y1 - dy, y1 + dy, y1, y1][res[0]].toFixed(3),
+          x3 = [0, 0, 0, 0, x4, x4, x4 - dx, x4 + dx][res[1]].toFixed(3),
+          y3 = [0, 0, 0, 0, y1 + dy, y1 - dy, y4, y4][res[1]].toFixed(3);
+      var path = ["M", x1.toFixed(3), y1.toFixed(3), "C", x2, y2, x3, y3, x4.toFixed(3), y4.toFixed(3)].join(",");
+      if (line && line.line) {
+          line.bg && line.bg.attr({path: path});
+          line.line.attr({path: path});
+      } else {
+          var color = typeof line == "string" ? line : "#000";
+          var strokeWidth = bg.split("|")[1] || 3;
+          var stroke = bg.split("|")[0];
+          return {
+              bg: bg && bg.split && this.path({stroke: stroke, fill: "none", "stroke-width": strokeWidth}, path),
+              line: this.path({stroke: color, fill: "none"}, path),
+              arrow: this.arrow(x3,y3,x4,y4,strokeWidth * 2.0,50,color),
+              from: obj1,
+              to: obj2
+          };
+      }
   };
   
   
   
   /**
+  * @constructor for Node
+  **/
+  MySystem.Node = function() {
+    this.terminals = [];
+    this.icon = [];
+    this.x = 0;
+    this.y = 0;
+  
+  };
+
+  /**
+  * @method terminal
+  * finds the named terminal in this node
+  * @param name of terminal to find
+  * @returns 'undefined' or the terminal
+  **/
+  MySystem.Node.prototype.terminal = function(name) {
+    var returnVal = null;
+    this.terminals.each(function(term){
+      if (term.name == name) {
+        returnVal =  term;
+      }
+    });
+    return returnVal;
+  };
+  
+  /**
   *
   **/
-  Node.importJson = function(jsonText) {
+  MySystem.Node.importJson = function(jsonText,contentBaseUrl) {
     var objs = eval(jsonText);
     var nodes = [];
     var wires = [];
     if (objs) {
       objs[0].containers.each(function(container) {
-        var node = new Node();
-        node.icon = new Image();
-        node.icon.src = container.icon;
+        var node = new MySystem.Node();
+        if (typeof(contentBaseUrl) != 'undefined') {
+          node.icon = contentBaseUrl + "/" + container.icon;
+        }
+        else {
+          node.icon = container.icon;
+        }
         node.x = (container.position[0].replace("px","")) / 1;
         node.y = (container.position[1].replace("px","")) / 1;
-        node.terminals = container.terminals;
         node.name = container.name;
-        node.icon.onload = function() {
-          debug("image loaded! for node " + node.name);
-          node.terminals.each(function(terminal) {
-            terminal.x = node.x;
-            terminal.y = node.y;
-            terminal.x += terminal.offsetPosition.left;
-            if (terminal.offsetPosition.top) {
-              terminal.y = terminal.y + terminal.offsetPosition.top;
-            }
-            else {
-              terminal.y = terminal.y - (terminal.offsetPosition.bottom - node.icon.height);
-            }
-          });
-          node.loaded = true;
-        };
-        var tryCount = 0;
+      
+        node.terminals = container.terminals;
         nodes.push(node);
       });
     }
@@ -53,101 +155,97 @@
   };
   
   
-  /**
-  *
-  **/
-  Node.prototype.paint = function(context) {
-      context.strokeStyle ='black';
-      if (this.loaded) {
-        context.drawImage(this.icon,this.x,this.y);
-        this.terminals.each(function(terminal) {
-          context.strokeRect(terminal.x,terminal.y,10,10);
-        });
-      }
-      else {
-        context.strokeRect(this.x,this.y,50,50);
-      }
-      context.drawText('whatever',7,this.x,this.y - 2,this.name);
-  };
-  
   
   /**
-  *
+  * @constructor for Wire
   **/
-  MySystemPrint = function(_ds) {
-    this.dataService = _ds;
-    this.data = [];
-    this.name = "my print";
+  MySystem.Wire = function() {
+    this.source = null;
+    this.target = null;
+    this.x = 0;
+    this.y = 0;
   };
 
   /**
-  *
+  * @method importJson
+  * creates wires from json text
   **/
-  MySystemPrint.prototype.drawBezierCurve = function(p1,p2,d1,d2,coeffMulDirection,ctxt) {
-    // Calcul des vecteurs directeurs d1 et d2 :
-    d1 = [d1[0]*coeffMulDirection, d1[1]*coeffMulDirection];
-    d2 = [d2[0]*coeffMulDirection, d2[1]*coeffMulDirection];
+  MySystem.Wire.importJson = function(jsonText,nodes) {
+    var objs = eval(jsonText);
+    var wires = [];
+    if (objs) {
+      objs[0].wires.each(function(w) {
+        var wire = new MySystem.Wire();
+        wire.src = w.src;
+        wire.sourceNode = nodes[w.src.moduleId];
+        wire.sourceTerminal = wire.sourceNode.terminal(w.src.terminal);
+        
+        wire.tgt = w.tgt;
+        wire.targetNode = nodes[w.tgt.moduleId];
+        wire.targetTerminal = wire.targetNode.terminal(w.tgt.terminal);
+        
+        wire.options = w.options;
+        wire.fields = w.options.fields;
+        wire.width = wire.fields.width;
+        wire.name = wire.fields.name;
+        wire.color = w.options.color;
+        wire.color.name = wire.fields.color;
+        
+        // do the drawing for the wire 
 
-    var bezierPoints=[];
-    bezierPoints[0] = p1;
-    bezierPoints[3] = p2;
-    bezierPoints[1] = [p1[0]+d1[0],p1[1]+d1[1]];
-    bezierPoints[2] = [p2[0]+d2[0],p2[1]+d2[1]];
-
-    // Draw the inner bezier curve
-    ctxt.lineCap = this.options.cap;
-    ctxt.strokeStyle = this.options.color;
-    ctxt.lineWidth = this.options.width;
-    ctxt.beginPath();
-    ctxt.moveTo(bezierPoints[0][0],bezierPoints[0][1]);
-    ctxt.bezierCurveTo(
-      bezierPoints[1][0],bezierPoints[1][1],
-      bezierPoints[2][0],bezierPoints[2][1],
-      bezierPoints[3][0],bezierPoints[3][1]
-    );
-    ctxt.stroke();
-  };
-
-
-  /**
-  *
-  **/
-  MySystemPrint.prototype.load = function() {
-    if (this.dataService) {
-      this.dataService.load(this,this.loadCallback);
-      setTimeout(this.show.bind(this),500);
-    }
-    else {
-      alert("No Data Service defined");
-    }
-  };
-
-
-  /**
-  *
-  **/
-  MySystemPrint.prototype.loadCallback = function(_data,context) {
-    context.nodes = Node.importJson(_data);
-  };
-
-
-  /**
-  *
-  **/
-  MySystemPrint.prototype.show = function() {
-    debug("show called for "+ this.name);
-    if (this.data) {
-      var canvas = $('printing_canvas');
-      var context = canvas.getContext("2d");
-      CanvasTextFunctions.enable (context);
-      context.fillStyle = "rgb(200,200,200)";
-      context.fillRect (0, 0, canvas.width, canvas.height);
-      this.nodes.each(function(node){
-        node.paint(context);
+        wires.push(wire);
       });
     }
-    else {
-      alert("No data found to MySystemPrint");
-    }
-  }; 
-})();
+    return wires;
+  };
+  
+    
+  /**
+  * @constructor
+  **/
+  MySystemPrint = function(_json,dom_id,contentBaseUrl,width,height) {
+    this.data = _json;
+    this.name = "my print";
+    this.scale = 0.6;
+    this.graphics = Raphael(document.getElementById(dom_id),width,height);
+    this.nodes = MySystem.Node.importJson(_json,contentBaseUrl);
+    this.wires = MySystem.Wire.importJson(_json,this.nodes);
+    this.visContent = this.graphics.set();
+    var self = this;
+    
+    this.nodes.each(function(node) {
+      self.drawNode(node);
+      self.visContent.push(node.rep);
+    });
+    
+    this.wires.each(function(wire) {
+      self.drawWire(wire);
+      self.visContent.push(wire.rep);
+    });
+    this.wrapper = this.graphics;
+  };
+  
+  /**
+  * @method drawNode
+  **/
+  MySystemPrint.prototype.drawNode = function(node) {
+    node.rep = this.graphics.set();
+    var x = node.x * this.scale;
+    var y = node.y * this.scale;
+    var size = 50 * this.scale;
+    node.rep.push(this.graphics.image(node.icon,x,y,size,size)); // TODO: Fixed size==BAD
+    var txt = this.graphics.text(x-10,y,node.name);
+    txt.rotate(-90,true);
+    node.rep.push(txt);
+  };
+  
+  /**
+  * @method drawWire
+  * IMPORTANT!!!!! This must be called *after* a call to drawNode. (HACKY-BAD)
+  **/
+  MySystemPrint.prototype.drawWire = function(wire) {
+    wire.rep = this.graphics.connection(wire.sourceNode.rep,wire.targetNode.rep,wire.color,wire.color + "|" + (wire.width * this.scale));
+  }
+  
+
+}());
