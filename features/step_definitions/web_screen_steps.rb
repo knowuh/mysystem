a33f20019296453224f7cd4644ff2f8b4b55cd2c
Seong-June Kim <jkim707@gmail.com>
2010-03-30 11:18:55 -0400

    Removed selenium client test

--- features/step_definitions/web_screen_steps.rb
+++ features/step_definitions/web_screen_steps.rb
@@ -13,7 +13,6 @@ Then /^(?:|I )should see "([^\"]*)" on the screen(?: within "([^\"]*)")?$/ do |t
   with_scope(selector) do
     puts("text=[#{text}]")
     elem = page.find(:xpath, "//*[contains(text(), '#{text}')]")
-    elem.should_not == nil
     elem.should be_visible
   end
 end
