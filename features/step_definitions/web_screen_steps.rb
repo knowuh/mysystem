b6cd50db4ea9056c03ad9d8ee2995df52b167e2c
Seong-June Kim <jkim707@gmail.com>
2010-03-30 12:02:06 -0400

    Marked the step as pending
    instead of commenting out the whole step

--- features/step_definitions/web_screen_steps.rb
+++ features/step_definitions/web_screen_steps.rb
@@ -9,10 +9,11 @@ When /^(?:|I )am on (.+)$/ do |page_name|
   visit path_to(page_name)
 end
 
-#Then /^(?:|I )should see "([^\"]*)" on the screen(?: within "([^\"]*)")?$/ do |text, selector|
-#  with_scope(selector) do
-#    puts("text=[#{text}]")
-#    elem = page.find(:xpath, "//*[contains(text(), '#{text}')]")
-#    elem.should be_visible
-#  end
-#end
+Then /^(?:|I )should see "([^\"]*)" on the screen(?: within "([^\"]*)")?$/ do |text, selector|
+  pending
+  with_scope(selector) do
+    puts("text=[#{text}]")
+    elem = page.find(:xpath, "//*[contains(text(), '#{text}')]")
+    elem.should be_visible
+  end
+end
