e57df2f29a1d0e578fdf9164b96b89bbddf1a3b0
Seong-June Kim <jkim707@gmail.com>
2010-04-15 14:45:18 -0400

    Forgot to commit with 0269aa04323ca5f2a90af0a5e619aae7d3035267

---
+++ src/js/mysystem-init.js
@@ -0,0 +1,40 @@
+(function() {
+    // Create a name space for MySystem
+    if (typeof mysystem === 'undefined' || !mysystem) {
+        this.mysystem = {};
+    }
+    
+    // In case console.log() is not defined
+    if (typeof console === 'undefined' || !console) {
+        this.console = {};
+    }
+    if (!console.log) {
+        console.log = function() {};
+    }
+    this.debug = console.log;
+    
+    $(document).ready(function() {
+        if (!mysystem.dataService) {
+            alert('Data service is undefined');
+            return;
+        }
+
+        var mySystem = new mysystem.MySystem('prefs.json');
+        mySystem.setDataService(mysystem.dataService);
+        if (mySystem.editor) {
+            mySystem.editor.enableLoadAndSave();
+        }
+        mySystem.load();
+        
+        $(window).unload(function() {});
+
+        var onExit = function () {
+            debug("saving mystem data before we leave");
+            mySystem.save();
+            debug("saved!");
+        }
+        //window.onbeforeunload = onExit;
+    });
+    
+})();
+
